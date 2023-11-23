e37de3d22eae96940118e14dd8f37b5ea94f7e78
zenitsuXdemon <pankaj.yadav.cse20@itbhu.ac.in>
2023-11-23 15:56:59 +0530

    added core-js and regenereator-runtime

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import './index.css'
 const Main = () => {
   return (
